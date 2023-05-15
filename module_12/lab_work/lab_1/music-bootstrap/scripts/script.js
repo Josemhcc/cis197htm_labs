e4f69f1da27925ca08f2b9d291f43ee6572a270e
Jose A <j.avendano2@saints.mhcc.edu>
2023-05-15 11:21:34 -0700

    Complete

---
+++ module_12/lab_work/lab_1/music-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Jose Avendano
+    File Name: script.js
+    Date: 5/8/2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
